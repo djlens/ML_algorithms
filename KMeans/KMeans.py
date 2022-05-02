503cb948ea305336f607ffef31f6129132525888
djlens <kuba.lenard@gmail.com>
2022-05-02 12:20:32 +0200

    added iterations to KMeans

--- KMeans/KMeans.py
+++ KMeans/KMeans.py
@@ -1,4 +1,16 @@
+import math
+
 import numpy as np
+
+
+def euclidean(p1, p2):
+    # compute distance between two points using Euclidean distance
+    assert len(p1) == len(p2)
+    return np.sqrt(np.sum((p1 - p2) ** 2))
+
+
+def calculate_distances_within_cluster(cluster, centroid):
+    return sum([euclidean(obs, centroid) ** 2 for obs in cluster])
 
 
 class KMeans:
@@ -6,48 +18,60 @@ class KMeans:
     def __init__(self, n_clusters):
         self.X = None
         self.y = None
+        self.y_ = None
+        self.wcss = 0
+        self.wcss_ = []
         self.centroids = None
+        self.centroids_ = None
         self.n_clusters = n_clusters
+        self._iter = None
+        self.loss = np.inf
+        self.prev_loss = np.inf
 
-    def fit(self, X):
-        self.X = X
-        self.y = np.zeros(self.X.shape[0])
+    def fit(self, X, no_of_iters=10):
+        self.y_ = [[] for _ in range(no_of_iters)]
+        self.centroids_ = [[] for _ in range(no_of_iters)]
+        for x in range(no_of_iters):
+            self._iter = x
+            self.X = X
+            self.y_[self._iter] = np.zeros(self.X.shape[0])
+            self.wcss = 0
 
-        # initialise centroids from randomly choosen points
-        rows_idx = np.random.choice(self.X.shape[0], self.n_clusters, replace=False)
-        self.centroids = self.X[rows_idx, :]
+            # initialise centroids from randomly choosen points
+            rows_idx = np.random.choice(self.X.shape[0], self.n_clusters, replace=False)
+            self.centroids_[self._iter] = self.X[rows_idx, :]
 
-        for i in range(100):
             self._assign_clusters()
             self._compute_centroids()
+
+            while self.prev_loss > self.loss:
+                self._assign_clusters()
+                self._compute_centroids()
+
+            self.wcss_.append(self.loss)
+
+        self.wcss = min(self.wcss_)
+        self.y = self.y_[np.argmax(self.wcss_)]
+        self.centroids = self.centroids_[np.argmax(self.wcss_)]
 
         return self
 
     def _assign_clusters(self):
         for index, obs in enumerate(self.X):
-            distances = [self._euclidean(obs, centroid) for centroid in self.centroids]
+            distances = [euclidean(obs, centroid) for centroid in self.centroids_[self._iter]]
             cluster = np.argmin(distances)
-            self.y[index] = cluster
+            if cluster != self.y_[self._iter][index]:
+                self.y_[self._iter][index] = cluster
 
     def _compute_centroids(self):
-        obs_with_clusters = list(zip(self.X, self.y))
-        loss = 0
+        obs_with_clusters = list(zip(self.X, self.y_[self._iter]))
+        self.prev_loss = self.loss
+        self.loss = 0
 
-        for index, centroid in enumerate(self.centroids):
+        for index, centroid in enumerate(self.centroids_[self._iter]):
             obs_with_clusters_copy = obs_with_clusters.copy()
             cluster = list(filter(lambda row: row[-1] == index, obs_with_clusters_copy))
             cluster, _ = list(zip(*cluster))
             new_centroid = np.mean(cluster, axis=0)
-            self.centroids[index] = new_centroid
-
-            loss += self.WCSS(cluster, new_centroid)
-
-        print(loss)
-
-    def WCSS(self, cluster, centroid):
-        return sum([self._euclidean(obs, centroid) for obs in cluster])
-
-    def _euclidean(self, p1, p2):
-        # compute distance between two points using Euclidean distance
-        assert len(p1) == len(p2)
-        return np.sqrt(np.sum((p1 - p2) ** 2))
+            self.centroids_[self._iter][index] = new_centroid
+            self.loss += calculate_distances_within_cluster(cluster, new_centroid)
--- KMeans/prepare_data.py
+++ KMeans/prepare_data.py
@@ -1,11 +1,16 @@
 import numpy as np
 from KMeans import KMeans
+from sklearn.cluster import KMeans as KMeansSK
 
 
 def prepare_data():
     wines = np.genfromtxt('./data/wine-clustering.csv', delimiter=',', skip_header=True)
     model = KMeans(n_clusters=3).fit(wines)
-    return model
+    model2 = KMeansSK(n_clusters=3, random_state=5).fit(wines)
+    print(model.wcss_)
+    print((model.wcss))
+    print("------")
+    print(model2.inertia_)
 
 
 if __name__ == "__main__":

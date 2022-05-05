ec8d6d70847ca0ce985c5863f8c4aa5a28339e2a
djlens <kuba.lenard@gmail.com>
2022-05-05 14:19:12 +0200

    finished

--- KMeans/main.py
+++ KMeans/main.py
@@ -5,8 +5,8 @@ from sklearn.cluster import KMeans as KMeansSK
 
 def test():
     wines = np.genfromtxt('./data/wine-clustering.csv', delimiter=',', skip_header=True)
-    model = KMeans(n_clusters=3).fit(wines)
-    model2 = KMeansSK(n_clusters=3).fit(wines)
+    model = KMeans(n_clusters=5).fit(wines, no_of_iters=1)
+    model2 = KMeansSK(n_clusters=5).fit(wines)
     print(model.wcss)
     print(model.y)
     print("------")
